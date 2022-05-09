2f5a9dea1ea660f056cb3a2c4865d9daf602e298
Ziheng Guo <527463513@qq.com>
2022-05-09 15:03:08 +0800

    path unified the unix

--- tools/data_converter/kitti_data_utils.py
+++ tools/data_converter/kitti_data_utils.py
@@ -29,9 +29,9 @@ def get_kitti_info_path(idx,
     img_idx_str += file_tail
     prefix = Path(prefix)
     if training:
-        file_path = Path('training') / info_type / img_idx_str
+        file_path = Path('training').joinpath(info_type,img_idx_str).as_posix()#Path('training') / info_type / img_idx_str
     else:
-        file_path = Path('testing') / info_type / img_idx_str
+        file_path = Path('testing').joinpath(info_type,img_idx_str).as_posix()#Path('testing') / info_type / img_idx_str
     if exist_check and not (prefix / file_path).exists():
         raise ValueError('file not exist: {}'.format(file_path))
     if relative_path:
